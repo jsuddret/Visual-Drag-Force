19145364ea37f564db0e7ab3b9958f394ade236a
Jake Suddreth <58892456+jsuddret@users.noreply.github.com>
2020-03-23 18:42:40 -0700

    Add files via upload

---
+++ DF.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+# retrieve values from list as float
+y = [float(line.strip()) for line in open('y_values_df.txt')]
+v = [float(line.strip()) for line in open('v_values_df.txt')]
+stages = []
+index = 0
+
+# create window, set attributes
+window = Tk()
+h = 1000
+w = 800
+canvas = Canvas(window, width=w, height=h, background='light blue')
+window.title('VGF')
+canvas.pack()
+
+wd2_s1 = 10
+wd2_s2 = 8
+wd2_s3 = 6
+h_s1 = 80
+h_s2 = 20
+stage1 = canvas.create_rectangle(w/2-wd2_s1, h, w/2+wd2_s1, h-h_s1, fill='black')
+stages.append(stage1)
+stage2 = canvas.create_rectangle(w/2-wd2_s2, h-h_s2, w/2+wd2_s2, h-h_s1-h_s2, fill='black')
+stages.append(stage2)
+stage3 = canvas.create_rectangle(w/2-wd2_s3, h-2*h_s2, w/2+wd2_s3, h-h_s1-2*h_s2, fill='black')
+stages.append(stage3)
+
+while index < len(y) - 1:
+    for stage in stages:
+        canvas.move(stage, 0, (y[index + 1] - y[index])*-1)
+    canvas.update()
+    index += 1
+
+window.mainloop()
---
+++ DragForce.m
@@ -0,0 +1,18 @@
+% by jake suddreth
+% warning('off')
+clc;
+time = 0:0.00001:0.435;
+% acceleration due to gravity 
+g = 32.174; % ft/s^2
+A = (pi/4)*(33^2); %ft^2
+C = 0.515; % coefficient of drag
+f = @(t, n) [n(2); ((7.6*10^6)-(103.44*exp(-4*10^(-5)*n(2))*C*A*n(1)^2)-(g*(6.2*10^6)-4*t*10^4))/((6.2*10^6)-4*t*10^4)];
+IC = [0, 1.4*10^3];
+[t, n] = ode45(f, time, IC);
+y = n(:,1);
+y_file = fopen('y_values_df.txt', 'w');
+fprintf(y_file, '%12.16f\n', y);
+fclose(y_file);
+y_file = fopen('v_values_df.txt', 'w');
+fprintf(y_file, '%12.16f\n', y);
+fclose(y_file);
