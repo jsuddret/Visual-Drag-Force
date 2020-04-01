8579112391d34b5f27283c0b22b9bb45ccb7858c
Jake Suddreth <58892456+jsuddret@users.noreply.github.com>
2020-04-01 00:04:44 -0700

    Add files via upload

--- DF.py
+++ DF.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 # retrieve values from list as float
 y = [float(line.strip()) for line in open('y_values_df.txt')]
-# d = [float(line.strip()) for line in open('d_values_df.txt')]
+d = [float(line.strip()) for line in open('d_values_df.txt')]
 y.reverse()
 stages = []
 index = 0
@@ -28,13 +28,16 @@ stage3 = canvas.create_rectangle(w/2-wd2_s3, h-2*h_s2, w/2+wd2_s3, h-h_s1-2*h_s2
 stages.append(stage3)
 exhaust = canvas.create_polygon(w/2-wd2_s1, h+2, w/2+wd2_s1, h+2, w/2+1, h+2+wd2_s1, fill='orange')
 stages.append(exhaust)
-velocity = canvas.create_text(725, 975, text='v='+str(round(y[len(y) - 1], 6)))
-# drag = canvas.create_text(725, 980, text='d='+str(round(d[0], 6)))
+velocity = canvas.create_text(725, 950, text='v='+str(round(y[len(y) - 1], 6)))
+v = 0
+drag = canvas.create_text(725, 975, text='d='+str(round(d[0], 6)))
 
-while index < len(y) - 1:
+while index < len(y) - 1 and v < 95:
     for stage in stages:
-        canvas.move(stage, 0, (y[index]*0.00000435)*(y[len(y) - 1] - y[len(y) - index - 1]))
-    canvas.itemconfigure(velocity, text='v='+str(round(y[len(y) - 1 - index], 6)))
+        canvas.move(stage, 0, 0.75*(y[index]*0.00000435)*(y[len(y) - 1] - y[len(y) - index - 1]))
+    canvas.itemconfigure(velocity, text='v='+str(round(y[len(y) - 1 - index], 6)) + ' m/s')
+    canvas.itemconfigure(drag, text='D='+str(round(d[index]*0.00000435*(0.75**2), 6)) + ' N')
+    v = y[len(y) - 1 - index]
     canvas.update()
     index += 1
 
